dcfeb8a72b193b668bd03c41474af0a650c13ae3
SpriteSay <793750155@qq.com>
2020-05-18 17:44:22 +0800

    添加了基本类和主要函数

---
+++ EmojiManagment/EmojiManagment/Emoji.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmojiManagment
+{
+    //表情类
+    class Emoji
+    {
+        public string Id { set; get; }
+        public string Path { set; get; }    //图片路径
+        public string Keyword { set; get; }    //关键词
+        public string Series { set; get; }      //图片所属系列
+        public string TargetPeople { set; get; }    //目标人群
+        public string Frequency { get; }    //使用频率
+
+        public Emoji() {}
+
+        public Emoji(string path, string keyword, string series, string targetpeople)
+        {
+            Path = path;
+            Keyword = keyword;
+            Series = series;
+            TargetPeople = targetpeople;
+        }
+
+        //判断文件类型，看到了现成的我就直接copy过来了
+        public bool IsImage(string path)
+        { // *.BMP;*.JPG;*.GIF;*.jpeg;*.ico
+            string ext = System.IO.Path.GetExtension(path).ToUpper();
+            if (ext == ".BMP" || ext == ".JPG" || ext == ".GIF"
+                 || ext == ".JPEG" || ext == ".ICO")
+            {
+                return true;//该文件是图像类型
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Id;
+        }
+
+
+
+
+    }
+}
---
+++ EmojiManagment/EmojiManagment/Favorate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmojiManagment
+{
+    //收藏夹类
+    class Favorate
+    {
+        public string Id { set; get; }
+
+        public Favorate() { }
+
+    }
+}
